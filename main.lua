f38ff8302f9cb0598c561a71d5728df9f467d872
SippingSizzurp <111225194+SippingSizzurp@users.noreply.github.com>
2023-04-16 02:22:52 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ local colorSettings =
 {
     ["Main"] = {
         ["HeaderColor"] = Color3.fromRGB(117, 0, 0),
-        ["HeaderShadingColor"] = Color3.fromRGB(117, 0, 0),
+        ["HeaderShadingColor"] = Color3.fromRGB(92, 0, 0),
         ["HeaderTextColor"] = Color3.fromRGB(255, 255, 255),
         ["MainBackgroundColor"] = Color3.fromRGB(47, 54, 64),
         ["InfoScrollingFrameBgColor"] = Color3.fromRGB(47, 54, 64),
